557797bac63e4e7f24e68722b0e00c09e82db216
domiebett <dbett49@gmail.com>
2019-07-03 21:23:22 +0300

    makes the api available for routing with zuul

--- src/middleware/config/Application.ts
+++ src/middleware/config/Application.ts
@@ -1,19 +1,19 @@
-import * as http from 'http2';
-import * as config from 'config';
+import * as fs from 'fs';
 import * as spdy from 'spdy';
 import * as path from 'path';
-import * as fs from 'fs';
-import { ExpressConfig } from './ExpressConfig';
-import { Eureka } from 'eureka-js-client';
+import * as http2 from 'http2';
+import * as config from 'config';
 import { Connection } from 'typeorm';
-import { Connector } from '../../data-layer/adapters/Connector';
+import { Eureka } from 'eureka-js-client';
 import { logger } from '../common/Logging';
-import { fstat } from 'fs';
+import { EurekaService } from './EurekaService';
+import { ExpressConfig } from './ExpressConfig';
+import { Connector } from '../../data-layer/adapters/Connector';
 
 export class Application {
     private express: ExpressConfig;
     private connection: Connection;
-    private server: http.Http2Server;
+    private server: http2.Http2Server;
     private client: Eureka;
     private port: number;
 
@@ -29,17 +29,18 @@ export class Application {
      */
     async setUpApplication() {
         this.connection = await this.createDatabaseConnection();
-        this.server = await this.serveExpressApp();
-        this.client = await this.setUpEureka();
+        // this.server = await this.serveExpressApp();
+        this.server = await this.serveHttpExpressApp();
+        this.client = await this.setUpEurekaRetry();
 
-        process.on('SIGINT', await this.closeConnections);
+        await process.on('SIGINT', await this.closeConnections);
     }
 
     /**
      * Create a connection to the database
      */
     private async createDatabaseConnection() {
-        logger.info('Creating database connection...');
+        await logger.info('Creating database connection...');
         return await Connector.connect();
     }
 
@@ -59,39 +60,41 @@ export class Application {
         });
     }
 
+    private async serveHttpExpressApp() {
+        return await this.express.app.listen(this.port, (error: any) => {
+            if (error) {
+                logger.error('Failed to start server with ssl', error);
+                return process.exit(1);
+            } else {
+                logger.info(`Server started on port ${this.port}`);
+            }
+        });
+    }
+
+    private async setUpEurekaRetry(timeout = 40000) {
+        await logger.info('Waiting for eureka to boot up.');
+
+        return await setTimeout(async () => {
+            try {
+                return await this.setUpEureka();
+            } catch(exception) {
+                await logger.info(`Reconnecting to eureka in ${(timeout * 2)/1000} seconds`);
+                return await this.setUpEurekaRetry(timeout * 2);
+            }
+        }, timeout);
+    }
+
     /**
      * Registers to Eureka
      */
     private async setUpEureka() {
-        const client: Eureka = new Eureka({
-            instance: {
-                app: config.get('app.name'),
-                hostName: this.server.address().address,
-                ipAddr: this.server.address().address,
-                statusPageUrl: `http://${this.server.address().address}:${this.port}`,
-                healthCheckUrl: `http://${this.server.address().address}:${this.port}/health`,
-                port: {
-                    '$': this.port,
-                    '@enabled': true
-                },
-                vipAddress: 'myvip',
-                dataCenterInfo: {
-                    '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-                    'name': 'MyOwn'
-                },
-            },
-            eureka: {
-                host: config.get('eureka.address') || 'eureka_server',
-                port: config.get('eureka.port') || 9091,
-                servicePath: '/eureka/apps/'
-            }
-        });
+        const client: Eureka = EurekaService.getClient();
         await logger.info('Connecting to eureka...');
 
         await client.start((error: Error) => {
             if (error && !error.message) {
                 if (!error.message) error.message = 'Error connecting to eureka server!!';
-                logger.info(error.message);
+                logger.error('Eureka Connection Error: ', error.message);
                 throw error;
             }
         });
