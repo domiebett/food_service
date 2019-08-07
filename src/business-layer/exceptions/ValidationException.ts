10f867b5b5f132b42de76eac58b2912ca2174cd3
domiebett <dbett49@gmail.com>
2019-08-07 18:48:22 +0300

    chore(error-handling): create custom error handler

--- src/business-layer/exceptions/ValidationException.ts
+++ src/business-layer/exceptions/ValidationException.ts
@@ -1,10 +1,11 @@
-export class ValidationException {
+export class ValidationException extends Error {
     constraints: any;
     status: number;
     name: string;
     message: string;
 
     constructor(constraints) {
+        super();
         this.name = 'Validation Error',
         this.message = 'Validation failed. Please check your inputs and try again',
         this.constraints = constraints;
