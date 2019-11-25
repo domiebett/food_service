3911b76cca495b4b525233126cab6dac3661ceb9
domiebett <dbett49@gmail.com>
2019-11-25 15:52:04 +0300

    update food-service

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,20 @@
 'use strict';
 
+// turn off logging
+const winston = require('winston');
+const logger = require('@bit/domiebett.budget_app.logging').logger;
+logger.remove(winston.transports.Console);
+
+// set env variables needed.
 process.env.APP_SECRET = 'EVNezN7pu1cS2iTSMJKsRtylBdN7izfH';
 process.env.NODE_ENV = 'test';
 
+// --------------------------------------------------------------- //
+// WARNING: Do not change any of the above settings unless you know
+// what you are doing.
+// --------------------------------------------------------------- //
+
+// mocha config.
 module.exports = {
 
 };
